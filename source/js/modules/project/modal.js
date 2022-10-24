a1e61d24bd5c350ee9a6f26982bfb6de42a52d4a
Alyona <alenka.inform@gmail.com>
2022-10-24 21:45:07 +0300

    Remove the focus outside the modal window

--- source/js/modules/project/modal.js
+++ source/js/modules/project/modal.js
@@ -1,14 +1,26 @@
 import {initForm} from './validation';
 
 const openModalWindow = () => {
+  const INTERECTIVE_ELEMENTS = ['button', 'a', 'input', 'textarea', '[tabindex]'];
+  const interactiveElementsArr = document.querySelectorAll(INTERECTIVE_ELEMENTS);
+
   const modalButton = document.querySelector('.header__button');
   modalButton.removeAttribute('href');
   const modalWindow = document.querySelector('.modal');
+  const modalInteractiveElements = modalWindow.querySelectorAll(INTERECTIVE_ELEMENTS);
   const closeButton = document.querySelector('.modal__close-button');
 
   modalButton.addEventListener('click', () => {
     document.body.style.position = 'fixed';
     modalWindow.classList.remove('modal--close');
+
+    interactiveElementsArr.forEach((element) => {
+      element.setAttribute('tabindex', '-1');
+    });
+
+    modalInteractiveElements.forEach((element) => {
+      element.setAttribute('tabindex', '1');
+    });
 
     const form = modalWindow.querySelector('.form-modal');
     const inputName = form.querySelector('input[type=text]');
@@ -19,6 +31,14 @@ const openModalWindow = () => {
   closeButton.addEventListener('click', () => {
     document.body.style.position = 'static';
     modalWindow.classList.add('modal--close');
+
+    interactiveElementsArr.forEach((element) => {
+      element.setAttribute('tabindex', '1');
+    });
+
+    modalInteractiveElements.forEach((element) => {
+      element.setAttribute('tabindex', '-1');
+    });
   });
 };
 
